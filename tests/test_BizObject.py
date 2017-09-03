46c559e78bb5824638fcf9fe5388672a77d4e54a
Jeff Doss <notdsk@gmail.com>
2017-09-03 11:43:31 -0400

    biz-default-tests: Reducing test structure complexity
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- tests/test_BizObject.py
+++ tests/test_BizObject.py
@@ -3,9 +3,9 @@ import os
 import pytest
 import mock
 
+from pybiz import schema as fields
 from pybiz.biz import BizObject, Relationship
 from pybiz.schema import Schema, Int, Str, Object
-from pybiz.test.fixtures.biz import BizFields
 
 
 @pytest.fixture(scope='module')
@@ -59,6 +59,15 @@ def Parent(SuperParent, Child):
         my_child = Relationship(Child)
 
     return Parent
+
+
+@pytest.fixture(scope='module')
+def BizFields():
+    class BizFields(BizObject):
+        floaty = fields.Float(allow_none=True)
+        floaty_default = fields.Float(default=7.77, allow_none=True)
+
+    return BizFields
 
 
 def test_BizObject_inherits_relationships(Parent, SuperParent):
