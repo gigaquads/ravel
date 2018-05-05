2e1295a286e6f7ec2198c5f98011cbb79dab6217
Jeff Doss <notdsk@gmail.com>
2018-05-05 18:14:33 -0400

    better-pyproj: Segmenting pyproj schema, adding module support
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/embryos/python_project/schema.py
+++ pybiz/embryos/python_project/schema.py
@@ -1,7 +1,5 @@
 from appyratus.validation import schema, fields
 
-from embryo import hooks
-
 
 class PythonProjectSchema(schema.Schema):
     name = fields.Str()
@@ -10,8 +8,3 @@ class PythonProjectSchema(schema.Schema):
     tagline = fields.Str()
     action = fields.Str()
     butts = fields.Str(default='wat')
-
-
-def pre_create(context):
-    context = hooks.PreCreateHook.from_schema(PythonProjectSchema, context)
-    return context
---
+++ pybiz/embryos/python_project/templates/{{ name|dash }}/{{ name|snake }}/base.py
@@ -0,0 +1,5 @@
+class {{ name|camel }}(object):
+    """
+    The {{ name|title }} class
+    """
+    pass
