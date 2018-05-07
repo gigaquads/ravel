7238c4e4ca9f626fc296057c616c07d294a9cc11
Jeff Doss <notdsk@gmail.com>
2018-05-07 08:21:57 -0400

    better-pyproj: Better exception for when manifest dao not defined
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/dao/base.py
+++ pybiz/dao/base.py
@@ -4,7 +4,6 @@ import venusian
 from abc import ABCMeta, abstractmethod
 
 
-
 class DAOError(Exception):
     pass
 
@@ -25,26 +24,26 @@ class Dao(object, metaclass=DaoMeta):
         pass
 
     @abstractmethod
-    def fetch(self, _id=None, public_id=None, fields: dict = None) -> dict:
+    def fetch(self, _id=None, public_id=None, fields: dict=None) -> dict:
         pass
 
     @abstractmethod
     def fetch_many(
-        self, _ids: list = None, public_ids: list = None, fields: dict = None
+        self, _ids: list=None, public_ids: list=None, fields: dict=None
     ) -> dict:
         pass
 
     @abstractmethod
-    def create(self, _id=None, public_id=None, data: dict = None) -> dict:
+    def create(self, _id=None, public_id=None, data: dict=None) -> dict:
         pass
 
     @abstractmethod
-    def update(self, _id=None, public_id=None, data: dict = None) -> dict:
+    def update(self, _id=None, public_id=None, data: dict=None) -> dict:
         pass
 
     @abstractmethod
     def update_many(
-        self, _ids: list = None, public_ids: list = None, data: list = None
+        self, _ids: list=None, public_ids: list=None, data: list=None
     ) -> dict:
         pass
 
@@ -53,10 +52,8 @@ class Dao(object, metaclass=DaoMeta):
         pass
 
     @abstractmethod
-    def delete_many(self, _ids: list = None, public_ids: list = None) -> dict:
+    def delete_many(self, _ids: list=None, public_ids: list=None) -> dict:
         pass
-
-
 
 
 class DaoManager(object):
@@ -76,5 +73,10 @@ class DaoManager(object):
         self._dao_classes[bizobj_class] = dao_class
 
     def get_dao(self, bizobj_class):
+        if bizobj_class not in self._dao_classes:
+            raise Exception(
+                'Unable to find "{}" in dao classes.  Hint: did you define a manifest?'.
+                format(bizobj_class.__name__)
+            )
         dao_class = self._dao_classes[bizobj_class]
         return dao_class()
