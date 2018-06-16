52aae98a7cb051677abd992016df396099d30413
Jeff Doss <notdsk@gmail.com>
2018-06-16 18:31:41 -0400

    better-pyproj: Upgrading python project to use latest Embryo structure
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/embryos/python_project/hooks.py
+++
@@ -1,8 +0,0 @@
-from embryo import hooks
-
-from pybiz.embryos.python_project.schema import PythonProjectSchema
-
-
-def pre_create(context):
-    context = hooks.PreCreateHook.from_schema(PythonProjectSchema, context)
-    return context
