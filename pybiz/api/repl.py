556efa429adefa699c706433343fd7ffd5fad036
Daniel Gabriele <d.gabri3le@gmail.com>
2018-07-16 16:47:26 -0400

    integation of CliProgram with Repl
    Signed-off-by: Daniel Gabriele <d.gabri3le@gmail.com>

--- pybiz/api/repl.py
+++ pybiz/api/repl.py
@@ -1,6 +1,9 @@
-from IPython.terminal.embed import InteractiveShellEmbed
+import inspect
 
-from .base import FunctionRegistry
+from IPython.terminal.embed import InteractiveShellEmbed
+from appyratus.cli import CliProgram, Subparser as Parser, Arg
+
+from .base import FunctionRegistry, FunctionDecorator, FunctionProxy
 
 
 class Repl(FunctionRegistry):
@@ -10,10 +13,20 @@ class Repl(FunctionRegistry):
     experimenting with an API from a command-line interface.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, name=None, version=None, tagline=None, defaults=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._ipython = InteractiveShellEmbed()
         self._proxies = []  # FunctionProxy objects wrap decorated functions
+        self._ipython = None
+        self._cli_program_kwargs = {
+            'name': name,
+            'version': version,
+            'tagline': tagline,
+            'defaults': defaults,
+        }
+
+    @property
+    def function_proxy_type(self):
+        return ReplFunctionProxy
 
     def hook(self, proxy):
         """
@@ -22,15 +35,45 @@ class Repl(FunctionRegistry):
         """
         self._proxies.append(proxy)
 
-    def start(self, *args, **kwargs):
+    def unpack(self, signature, *args, **kwargs):
+        if args and isinstance(args[0], CliProgram):
+            prog = args[0]
+            arguments = prog.args
+            args, kwargs = [], {}
+
+            for k, param in signature.parameters.items():
+                value = getattr(arguments, k, None)
+                if param.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD:
+                    if param.default is inspect._empty:
+                        args.append(value)
+                    else:
+                        kwargs[k] = value
+                elif param.kind == inspect.Parameter.POSITIONAL_ONLY:
+                    args.append(value)
+                elif param.kind == inspect.Parameter.KEYWORD_ONLY:
+                    kwargs[k] = value
+
+        return (args, kwargs)
+
+    def start(self, interactive=True, *args, **kwargs):
         """
         Start a new REPL with all registered functions available in the REPL
         namespace.
         """
-        self._ipython.mainloop(local_ns=dict(
-            {p.func_name: p.func for p in self._proxies},
-            repl=self
-        ))
+        if interactive:
+            self._ipython = InteractiveShellEmbed()
+            self._ipython.mainloop(local_ns=dict(
+                {p.func_name: p.func for p in self._proxies},
+                repl=self
+            ))
+        else:
+            class DynamicCliProgram(CliProgram):
+                @staticmethod
+                def subparsers():
+                    return [p.parser for p in self._proxies if p.parser]
+
+            prog = DynamicCliProgram(**self._cli_program_kwargs)
+            prog.run()
 
     @property
     def function_names(self):
@@ -39,3 +82,10 @@ class Repl(FunctionRegistry):
         """
         return sorted(p.func_name for p in self._proxies)
 
+
+class ReplFunctionProxy(FunctionProxy):
+    def __init__(self, func, decorator):
+        super().__init__(func, decorator)
+        self.parser = decorator.params.get('parser')
+        if self.parser is not None:
+            self.parser.perform = self
--- pybiz/examples/example_repl.py
+++ pybiz/examples/example_repl.py
@@ -1,18 +1,45 @@
-from pybiz.api.repl import Repl
+import sys
+
+from pybiz.api.repl import Repl, Parser, Arg
 
 
-api = Repl()
+api = Repl(name='Repl Test', version='1.0.0')
+
+
+@api(
+    usage='greet a person by their name',
+    parser=Parser(
+        name='greet',
+        usage='greet a person by their name',
+        args=[
+            Arg(
+                flags=('--name', ),
+                dtype=str,
+                usage="name of person to greet"
+            ),
+            Arg(
+                flags=('--intensity', ),
+                dtype=int,
+                usage="how angry you are",
+                default=1,
+            ),
+        ]
+    )
+)
+def greet(name, intensity=1):
+    if intensity > 1:
+        print('{} must be destroyed!'.format(name.title()))
+    else:
+        print('Hello, {}!'.format(name.title()))
 
 
 @api()
 def speak():
-    print('woof')
-
-
-@api()
-def greet(name):
-    print('hello, {}'.format(name))
+    print('Woof Woof!')
 
 
 if __name__ == '__main__':
-    api.start()
+    # use the presence of CLI args to indicate that
+    # this REPL should be run as a CLI program
+    has_cli_args = len(sys.argv) > 2
+    api.start(interactive=(not has_cli_args))
