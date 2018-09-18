c5528d8dd49a47442c3c680ff8c6a9c4b5168983
Jeff Doss <notdsk@gmail.com>
2018-09-18 19:24:12 -0400

    better-embryos: Tidying up
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/embryos/python-project/base/templates/setup.py
+++ pybiz/embryos/python-project/base/templates/setup.py
@@ -5,15 +5,14 @@ import os
 
 from appyratus.util import RealSetup
 
-if __name__ == '__main__':
-    setup = RealSetup(
-        path=os.path.abspath(os.path.dirname(__file__)),
-        name='{{ name }}',
-        version='{{ version }}',
-        description='{{ description }}',
-        author='{{ author }}',
-        author_email='{{ author_email }}',
-        url='{{ url }}',
-        classifiers=[],
-    )
-    setup.run()
+setup = RealSetup(
+    path=os.path.abspath(os.path.dirname(__file__)),
+    name='{{ name }}',
+    version='{{ version }}',
+    description='{{ description }}',
+    author='{{ author }}',
+    author_email='{{ author_email }}',
+    url='{{ url }}',
+    classifiers=[],
+)
+setup.run()
