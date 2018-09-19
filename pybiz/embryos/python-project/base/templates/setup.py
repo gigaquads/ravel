1c2acefc52b4e5fcc4d645249643fac7329cc624
Jeff Doss <notdsk@gmail.com>
2018-09-18 23:49:16 -0400

    better-embryos: Py proj license, setup classifiers
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/embryos/python-project/base/templates/setup.py
+++ pybiz/embryos/python-project/base/templates/setup.py
@@ -7,12 +7,12 @@ from appyratus.util import RealSetup
 
 setup = RealSetup(
     path=os.path.abspath(os.path.dirname(__file__)),
-    name='{{ name }}',
+    name='{{ name|snake }}',
     version='{{ version }}',
     description='{{ description }}',
     author='{{ author }}',
     author_email='{{ author_email }}',
     url='{{ url }}',
-    classifiers=[],
+    classifiers=['python3']
 )
 setup.run()
