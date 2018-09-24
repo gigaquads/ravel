bdf8d2e5e0e0c9e2e393bb882d940f8f22aa5738
Jeff Doss <notdsk@gmail.com>
2018-09-24 13:48:28 -0400

    misc-fixes: Fixing up setup
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+# vim: set filetype=python
 import os
 
-from setuptools import setup, find_packages
+from appyratus.util import RealSetup
 
-
-if __name__ == '__main__':
-    HERE = os.path.abspath(os.path.dirname(__file__))
-
-    with open(os.path.join(HERE, 'README.md')) as f:
-        README = f.read()
-
-    with open(os.path.join(HERE, 'requirements.txt')) as f:
-        REQUIREMENTS = [s.strip().replace('-', '_') for s in f.readlines()]
-
-    setup(name='pybiz',
-          version='1.0',
-          description='PyBiz',
-          long_description=README,
-          install_requires=REQUIREMENTS,
-          packages=find_packages(),
-          )
+setup = RealSetup(
+    path=os.path.abspath(os.path.dirname(__file__)),
+    name='pybiz',
+    version='0b0',
+    description='Pybiz',
+    author='Gigaquads',
+    author_email='notdsk@gmail.com',
+    url='https://github.com/gigaquads/pybiz',
+    classifiers=['python3']
+)
+setup.run()
