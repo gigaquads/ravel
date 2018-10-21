944ce74b68a0e8fe1109c2e4a106699463699faf
Jeff Doss <notdsk@gmail.com>
2018-10-21 15:46:17 -0400

    revert-setup: The realest setup unfortunately
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 # encoding=utf-8
-# vim: set filetype=python
-import os
+from setuptools import setup
 
-from appyratus.util import RealSetup
-
-setup = RealSetup(
-    path=os.path.abspath(os.path.dirname(__file__)),
-    name='pybiz',
-    version='0b0',
-    description='Pybiz',
-    author='Gigaquads',
-    author_email='notdsk@gmail.com',
-    url='https://github.com/gigaquads/pybiz.git',
-    classifiers=['python3']
-)
-setup.run()
+if __name__ == '__main__':
+    setup()
