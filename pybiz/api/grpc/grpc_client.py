d4de7720b3c685def1e46eba9d6f1b9fe3a34bfb
Jeff Doss <notdsk@gmail.com>
2019-02-01 03:40:10 -0500

    schema.v2.integrate: Supporting lists (and recursion) for grpc schema response data extraction
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/api/grpc/grpc_client.py
+++ pybiz/api/grpc/grpc_client.py
@@ -36,19 +36,18 @@ class GrpcClient(object):
         request_type = getattr(self._registry.pb2, '{}Request'.format(key))
         send_request = getattr(self._grpc_stub, proxy.name)
 
-        def func(**kwargs):
-            # prepare and send the request
-            req = request_type(**kwargs)
-            resp = send_request(req)
-            # translate the native proto response message to a plain dict
+        def extract_schema_data(data, data_fields):
             result = {}
-            for field_name, field in proxy.response_schema.fields.items():
-                value = getattr(resp, field_name, None)
+            for field_name, field in data_fields.items():
+                value = getattr(data, field_name, None)
                 if value is None:
                     result[field_name] = None
                 elif isinstance(field, fields.Dict):
                     decoded_value = codecs.decode(value, 'base64')
                     result[field_name] = pickle.loads(decoded_value)
+                elif isinstance(field, fields.List):
+                    nested_fields = field.nested.fields
+                    result[field_name] = [extract_schema_data(v, nested_fields) for v in value]
                 # the following should be enabled and expanded when this function becomes recursive
                 #elif isinstance(field, Schema):
                 #elif isinstance(field, fields.Nested)
@@ -56,6 +55,14 @@ class GrpcClient(object):
                 else:
                     result[field_name] = value
             # return the response dict
+            return result
+
+        def func(**kwargs):
+            # prepare and send the request
+            req = request_type(**kwargs)
+            resp = send_request(req)
+            # translate the native proto response message to a plain dict
+            result = extract_schema_data(resp, proxy.response_schema.fields)
             return result
 
         return func
