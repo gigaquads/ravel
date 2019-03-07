5eb9f934d8126a5382cc3a523aec5f4b5c8a1946
Jeff Doss <notdsk@gmail.com>
2019-03-06 22:32:18 -0500

    schema.v2.integrate: Cleaning up registry router
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz/util/registry_router.py
+++ pybiz/util/registry_router.py
@@ -1,6 +1,6 @@
 import sys
 
-from typing import Text, Dict
+from typing import Text, Dict, List
 
 from appyratus.cli import CliProgram, PositionalArg
 
@@ -12,7 +12,13 @@ class RegistryRouter(CliProgram):
     registries through a single command-line interface
     """
 
-    def __init__(self, manifest: 'Manifest' = None, *args, **kwargs):
+    def __init__(
+        self,
+        manifest: 'Manifest' = None,
+        registries: List = None,
+        *args,
+        **kwargs
+    ):
         """
         # Init
         Do not merge unknown args into the args dict, as the registry router
@@ -20,6 +26,7 @@ class RegistryRouter(CliProgram):
         """
         super().__init__(merge_unknown=False, *args, **kwargs)
         self._manifest = manifest
+        self._registries = registries
 
     def args(self):
         """
@@ -32,23 +39,45 @@ class RegistryRouter(CliProgram):
             PositionalArg(name='registry', usage='the registry to utilize')
         ]
 
-    def perform(self, program):
+    def perform(self, program: 'CliProgram'):
         """
         # Perform routing
         Route to the registry provided in the CLI's first argument
         """
         registry_name = self.cli_args.registry
-        if not hasattr(self, registry_name):
-            raise Exception('Unknown registry "{}"'.format(registry_name))
-        registry = getattr(self, registry_name)()
+        registry = self.get_registry(registry_name)
+        if not registry:
+            raise Exception(f'Unable to locate registry "{registry_name}"')
+        registry_method = getattr(self, f'run_{registry_name}', None)
+        if callable(registry_method):
+            return registry_method(registry)
+        else:
+            self.registry_lifecycle(registry=registry)
 
-    def run_registry(
+    def get_registry(self, name: Text):
+        """
+        # Get registry
+        First by registries dictionary (provided when initialized)
+        And if not there, then an attribute on this your router class
+        """
+        registry_dict = self._registries.get(name)
+        registry_attr = getattr(self, name, None)
+        if registry_dict:
+            return registry_dict
+        elif registry_attr:
+            return registry_attr()
+
+    def registry_lifecycle(
         self,
         registry: 'Registry',
         manifest: Text = None,
         bootstrap_kwargs: Dict = None,
         start_kwargs: Dict = None
     ):
+        """
+        Registry lifecycle
+        Perform necessary bootstrapping with manifest and then fire it up
+        """
         registry.bootstrap(
             manifest=manifest or self._manifest, **(bootstrap_kwargs or {})
         )
@@ -56,7 +85,12 @@ class RegistryRouter(CliProgram):
         return registry
 
     def run_cli(self, cli_registry: 'CliRegistry'):
-        return self.run_registry(
+        """
+        # Run Cli
+        A special implementation for the cli registry to provide this program's
+        unknown cli args to the cli registry program.
+        """
+        return self.registry_lifecycle(
             registry=cli_registry,
             bootstrap_kwargs={'cli_args': self._unknown_cli_args}
         )
