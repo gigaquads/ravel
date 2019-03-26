9ca539ada77e971a65c4f6badbca1451d3e737ae
Daniel Gabriele <d.gabri3le@gmail.com>
2019-03-25 23:13:28 -0400

    more logging and exceptions

--- pybiz/api/exc.py
+++ pybiz/api/exc.py
@@ -15,3 +15,11 @@ class GuardFailed(PybizError):
     def __init__(self, guard):
         super().__init__(f'failed guard: {guard.display_string}')
         self.guard = guard
+
+
+class NotAuthorized(PybizError):
+    pass
+
+
+class ValidationError(PybizError):
+    pass
