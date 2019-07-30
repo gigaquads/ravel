aad78714e60e002c849aeddc7de1815e7470edcc
Daniel Gabriele <d.gabri3le@gmail.com>
2019-07-30 17:03:30 -0400

    fixed import error
    Signed-off-by: Daniel Gabriele <d.gabri3le@gmail.com>

--- pybiz/api/web/__init__.py
+++ pybiz/api/web/__init__.py
@@ -2,4 +2,3 @@ from .http_registry import HttpRegistry, HttpRoute
 from .http_server_registry import HttpServerRegistry
 from .wsgi_service_registry import WsgiServiceRegistry
 from .websocket_server_registry import WebsocketServerRegistry
-from .graphql import GraphQLExecutor
