2fed6a56bd5564ae4fa4e6457b1a0fbbdcff39f9
Daniel Gabriele <d.gabri3le@gmail.com>
2020-02-22 01:05:04 -0500

    fixed Batch tests
    Signed-off-by: Daniel Gabriele <d.gabri3le@gmail.com>

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -6,73 +6,76 @@ from appyratus.utils import DictObject
 from ravel import String, Int, Float, Bool
 from ravel.batch import Batch, BatchResolverProperty
 
+@pytest.fixture(scope='function')
+def trees(Tree):
+    return Tree.Batch.generate(count=5).create()
 
 
-def test_factory_method(BasicResource, BasicBatch, basic_batch):
-    assert isinstance(getattr(BasicBatch, 'ravel', None), DictObject)
-    assert BasicBatch.ravel.owner is BasicResource
-    assert BasicBatch.ravel.indexed_field_types \
-            == BasicBatch.get_indexed_field_types()
-    assert BasicBatch.ravel.properties
-    assert len(BasicBatch.ravel.properties) == len({
-        resolver for resolver in BasicResource.ravel.resolvers.fields.values()
-        if isinstance(resolver.field, BasicBatch.get_indexed_field_types())
+def test_factory_method(Tree, trees):
+    assert isinstance(getattr(Tree.Batch, 'ravel', None), DictObject)
+    assert Tree.Batch.ravel.owner is Tree
+    assert Tree.Batch.ravel.indexed_field_types \
+            == Tree.Batch.get_indexed_field_types()
+    assert Tree.Batch.ravel.properties
+    assert len(Tree.Batch.ravel.properties) == len({
+        resolver for resolver in Tree.ravel.resolvers.fields.values()
+        if isinstance(resolver.field, Tree.Batch.get_indexed_field_types())
     })
 
     # ensure that BatchResolverProperty are created for each indexable
     # field type on its associated Resource class.
-    for resolver in BasicResource.ravel.resolvers.fields.values():
+    for resolver in Tree.ravel.resolvers.fields.values():
         if isinstance(resolver.field, Batch.get_indexed_field_types()):
-            attr = getattr(BasicBatch, resolver.name, None)
+            attr = getattr(Tree.Batch, resolver.name, None)
             assert isinstance(attr, BatchResolverProperty)
             print(
                 f'Batch class has expected resolver property: {resolver.name}'
             )
 
-    basic_batch = BasicBatch()
+    trees = Tree.Batch()
 
-    for resolver in BasicResource.ravel.resolvers.fields.values():
+    for resolver in Tree.ravel.resolvers.fields.values():
         if isinstance(resolver.field, Batch.get_indexed_field_types()):
-            assert resolver.name in basic_batch.internal.indexes
+            assert resolver.name in trees.internal.indexes
             print(f'Batch object has expected index: {resolver.name}')
         else:
-            assert resolver.name not in basic_batch.internal.indexes
+            assert resolver.name not in trees.internal.indexes
 
 
-def test_insert(basic_resource, basic_batch):
-    assert not len(basic_batch)
+def test_insert(basic_resource, trees):
+    assert not len(trees)
 
-    basic_batch.insert(0, basic_resource)
+    trees.insert(0, basic_resource)
 
-    assert len(basic_batch) == 1
-    assert basic_batch.internal.resources[0] is basic_resource
-    for k in basic_batch.ravel.properties:
+    assert len(trees) == 1
+    assert trees.internal.resources[0] is basic_resource
+    for k in trees.ravel.properties:
         v = basic_resource[k]
-        assert basic_batch.internal.indexes[k][v] == {basic_resource}
+        assert trees.internal.indexes[k][v] == {basic_resource}
 
-def test_insert(BasicResource, basic_batch): pass
-def test_append(BasicResource, basic_batch): pass
-def test_extend(BasicResource, basic_batch): pass
-def test_appendleft(BasicResource, basic_batch): pass
-def test_extendleft(BasicResource, basic_batch): pass
-def test_pop(BasicResource, basic_batch): pass
-def test_popleft(BasicResource, basic_batch): pass
+def test_insert(Tree, trees): pass
+def test_append(Tree, trees): pass
+def test_extend(Tree, trees): pass
+def test_appendleft(Tree, trees): pass
+def test_extendleft(Tree, trees): pass
+def test_pop(Tree, trees): pass
+def test_popleft(Tree, trees): pass
 
-def test_where(BasicResource, basic_batch):
+def test_where(Tree, trees):
     names = ['A', 'B', 'C']
-    basic_batch.extend([
-        BasicResource(required_str_field=name).create()
+    trees.extend([
+        Tree(name=name).create()
         for name in names
     ])
 
     for name in names:
-        predicate = BasicResource.required_str_field == name
-        filtered_batch = basic_batch.where(predicate)
+        predicate = Tree.name == name
+        filtered_batch = trees.where(predicate)
         assert filtered_batch.internal.indexed == False
         assert len(filtered_batch) == 1
         filtered_resource = filtered_batch.internal.resources[0]
-        assert filtered_resource.required_str_field == name
+        assert filtered_resource.name == name
 
         # do the same thing but make sure "indexed" is set.
-        filtered_batch = basic_batch.where(predicate, indexed=True)
+        filtered_batch = trees.where(predicate, indexed=True)
         assert filtered_batch.internal.indexed == True
