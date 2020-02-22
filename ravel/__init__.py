7d97d51663e43a57334f62462c23016979070d16
Jeff Doss <notdsk@gmail.com>
2020-02-22 18:48:27 +0000

    v2: Reinstating embryo context with destination
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- ravel/__init__.py
+++ ravel/__init__.py
@@ -1,26 +1,12 @@
 import typing
-
 from .schema import *
-
 from ravel.manifest import Manifest
 from ravel.logging import ConsoleLoggerInterface
 from ravel.store.base.store import Store
 from ravel.store.base.store_history import StoreEvent, StoreHistory
 from ravel.api import Api
-from ravel.app.base import (
-    Application,
-    EndpointDecorator,
-    Endpoint,
-)
-
-from ravel.app.apps import (
-    CliApplication,
-    HttpServer,
-    WebsocketServer,
-    AsyncServer,
-    Repl,
-)
-
+from ravel.app.base import Application, EndpointDecorator, Endpoint
+from ravel.app.apps import CliApplication, HttpServer, WebsocketServer, AsyncServer, Repl
 from ravel.resource import Resource
 from ravel.entity import Entity
 from ravel.batch import Batch
@@ -31,15 +17,11 @@ from ravel.query.query import Query
 from ravel.query.mode import QueryMode
 from ravel.query.order_by import OrderBy
 from ravel.query.request import Request
-from ravel.query.predicate import (
-    Predicate, ConditionalPredicate, BooleanPredicate
-)
+from ravel.query.predicate import Predicate, ConditionalPredicate, BooleanPredicate
 from ravel.resolver.resolver import Resolver
 from ravel.resolver.resolver_decorator import ResolverDecorator
 from ravel.resolver.resolver_property import ResolverProperty
 from ravel.resolver.resolver_manager import ResolverManager
 from ravel.resolver.resolvers.loader import Loader, LoaderProperty
 from ravel.resolver.resolvers.relationship import Relationship
-from ravel.resolver.decorators import (
-    resolver, relationship, view, nested,
-)
+from ravel.resolver.decorators import resolver, relationship, view, nested
